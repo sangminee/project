00ac993b1bc404410e2b617ab35bc12a4accf81d
sangminee <lsmlee99@gmail.com>
2023-09-28 05:49:54 +0900

    feat : PostEntity, UserEntity 생성

---
+++ src/main/java/com/example/project/model/UserStatus.java
@@ -0,0 +1,5 @@
+package com.example.project.model;
+
+public enum UserStatus {
+    PENDING, INACTIVE, ACTIVE
+}
---
+++ src/main/java/com/example/project/repository/PostEntity.java
@@ -0,0 +1,28 @@
+package com.example.project.repository;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "posts")
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at")
+    private Long createdAt;
+
+    @Column(name = "modified_at")
+    private Long modifiedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity writer;
+
+}
