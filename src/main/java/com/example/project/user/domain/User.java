00cb9f15a3ad733aa53772cd3ed7ce4e6339305f
sangminee <lsmlee99@gmail.com>
2023-10-05 01:52:44 +0900

    refactor(user) : 도메인 객체로 분리

---
+++ src/main/java/com/example/project/user/domain/User.java
@@ -0,0 +1,40 @@
+package com.example.project.user.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class User {
+
+    private final Long id;
+    private final String email;
+    private final String nickname;
+    private final String address;
+    private final String certificationCode;
+    private final UserStatus status;
+    private final Long lastLoginAt;
+
+    @Builder
+    public User(Long id, String email, String nickname, String address, String certificationCode, UserStatus status, Long lastLoginAt) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.address = address;
+        this.certificationCode = certificationCode;
+        this.status = status;
+        this.lastLoginAt = lastLoginAt;
+    }
+
+    public static User from(UserCreateDto userCreateDto){ // User 객체를 만들어 주는
+        return User.builder()
+                .email(userCreateDto.getEmail())
+                .nickname(userCreateDto.getNickname())
+                .address(userCreateDto.getAddress())
+                .status(UserStatus.PENDING)
+                .certificationCode(UUID.randomUUID().toString())
+                .build();
+    }
+
+}
--- src/main/java/com/example/project/user/infrastructure/UserEntity.java
+++ src/main/java/com/example/project/user/infrastructure/UserEntity.java
@@ -1,5 +1,6 @@
 package com.example.project.user.infrastructure;
 
+import com.example.project.user.domain.User;
 import com.example.project.user.domain.UserStatus;
 import com.example.project.user.domain.UserCreateDto;
 import jakarta.persistence.*;
@@ -31,14 +32,14 @@ public class UserEntity {
     private String address;
 
     @Column(name = "certification_code")
-    private String certificationCode;
+    private String certificationCode; // 주민 번호
 
     @Column(name = "status")
     @Enumerated(EnumType.STRING)
     private UserStatus status;
 
     @Column(name = "last_login_at")
-    private Long lastLoginAt;
+    private Long lastLoginAt; // 마지막 로그인 일시
 
     @Builder
     public UserEntity(String email, String nickname, String address, String certificationCode, UserStatus status) {
@@ -49,13 +50,27 @@ public class UserEntity {
         this.status = status;
     }
 
-    public static UserEntity of(UserCreateDto userCreateDto){
-        return UserEntity.builder()
-                .email(userCreateDto.getEmail())
-                .nickname(userCreateDto.getNickname())
-                .address(userCreateDto.getAddress())
-                .status(UserStatus.PENDING)
-                .certificationCode(UUID.randomUUID().toString())
+    public static UserEntity fromModel(User user) { // User 객체로 UserEntity 객체 생성
+        UserEntity userEntity = new UserEntity();
+        userEntity.id = user.getId();
+        userEntity.email = user.getEmail();
+        userEntity.nickname = user.getNickname();
+        userEntity.address = user.getAddress();
+        userEntity.certificationCode = user.getCertificationCode();
+        userEntity.status = user.getStatus();
+        userEntity.lastLoginAt = user.getLastLoginAt();
+        return userEntity;
+    }
+
+    public User toModel() { // UserEntity 객체를 가지고 User 객체 생성
+        return User.builder()
+                .id(id)
+                .email(email)
+                .nickname(nickname)
+                .address(address)
+                .certificationCode(certificationCode)
+                .status(status)
+                .lastLoginAt(lastLoginAt)
                 .build();
     }
 }
