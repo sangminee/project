f3fcf16cdd2fdc85196c037ecf0e4c91b65f8232
sangminee <lsmlee99@gmail.com>
2023-10-05 21:18:38 +0900

    feat(user-create) : User 생성 기본 함수

--- src/test/java/com/example/project/user/controller/UserCreateControllerTest.java
+++ src/test/java/com/example/project/user/controller/UserCreateControllerTest.java
@@ -4,6 +4,9 @@ import com.example.project.user.controller.response.UserResponse;
 import com.example.project.user.domain.UserCreate;
 import com.example.project.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UserCreateControllerTest{
@@ -11,6 +14,7 @@ class UserCreateControllerTest{
     @Test
     void UserCreate를_가지고_UserResponse를_생성할_수_있다(){
         // given
+        UserCreateController userCreateController = UserCreateController.builder().build();
         UserCreate userCreate = UserCreate.builder()
                 .email("test123@gmail.com")
                 .nickname("test123")
@@ -18,21 +22,16 @@ class UserCreateControllerTest{
                 .build();
 
         // when
-        UserResponse userResponse = UserResponse.builder()
-                .id(1L)
-                .email(userCreate.getEmail())
-                .nickname(userCreate.getNickname())
-                .status(UserStatus.PENDING)
-                .lastLoginAt(0L)
-                .build();
+        ResponseEntity<UserResponse> result = userCreateController.create(userCreate);
 
         // then
-        assertThat(userResponse).isNotNull();
-        assertThat(userResponse.getId()).isEqualTo(1L);
-        assertThat(userResponse.getEmail()).isEqualTo("test123@gmail.com");
-        assertThat(userResponse.getNickname()).isEqualTo("test123");
-        assertThat(userResponse.getStatus()).isEqualTo(UserStatus.PENDING);
-        assertThat(userResponse.getLastLoginAt()).isEqualTo(0L);
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(201));
+        assertThat(result.getBody()).isNotNull();
+        assertThat(result.getBody().getId()).isEqualTo(1L);
+        assertThat(result.getBody().getEmail()).isEqualTo("test123@gmail.com");
+        assertThat(result.getBody().getNickname()).isEqualTo("test123");
+        assertThat(result.getBody().getStatus()).isEqualTo(UserStatus.PENDING);
+        assertThat(result.getBody().getLastLoginAt()).isEqualTo(0L);
     }
 
 }
